6d1ed4792040a3deb1dd90c4d4d73dc3f9022554
Cloud User <linux@agent-9h5h3o4c.novalocal>
2017-09-21 11:09:36 +0000

    instance-metadata URL as env-var

---
+++ demo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+BUILD=$(git rev-parse --short HEAD)
+
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+docker run \
+  --rm -it \
+  -e DATAHUB_USER=XXXXXX \
+  -e DATAHUB_PW=XXXXXXX \
+  -e TARGET_BUCKET_PREFIX="s3://sentinel3-rbt/relorbit" \
+  -e SOURCE_PRODUCT="https://scihub.copernicus.eu/s3/odata/v1/Products('6b2aa89e-7fbf-4e05-b570-db9310edbdbe')/\$value" \
+asamerh4/sentinel3-s3-ingester:$BUILD
